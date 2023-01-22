da55803391dda3f16741b8f1b4b80da871d813bd
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-22 12:48:38 +0800

    Printing Statements / Outputs

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,15 +1,25 @@
 #Start
-numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
+
+print("Array values: [60, 63, 96, 5, 90, 43, 75, 41, 99, 23] ")
+
 def sort(numbers):
-   for i in range(10):
+
+   for i in range(9):
     minpos = i
-    for j in range (i,11):
-        if numbers[j] <numbers [minpos]:
+    for j in range (i,10):
+        if numbers[j] < numbers[minpos]:
             minpos = j
 
     #For the array values to swap.
     temp = numbers[i]
-    numbers [i] = numbers [minpos]
+    numbers[i] = numbers[minpos]
     numbers[minpos] = temp
-    
+    print(numbers)
+
+numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
+sort(numbers)
+
+
+print(numbers)
+
 
