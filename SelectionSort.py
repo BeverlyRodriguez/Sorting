97e5d2c44eee21e2e9db1d15c50f29ada649cf26
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-22 12:30:51 +0800

    For the array values to swap

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,4 +1,15 @@
 #Start
 numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
 def sort(numbers):
-   
+   for i in range(10):
+    minpos = i
+    for j in range (i,11):
+        if numbers[j] <numbers [minpos]:
+            minpos = j
+
+    #For the array values to swap.
+    temp = numbers[i]
+    numbers [i] = numbers [minpos]
+    numbers[minpos] = temp
+    
+
