2db3e3457c0b1ac051e80c6d2ffd5472c2f85279
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-22 23:19:25 +0800

    Adding Comments and Programmed by:Rodriguez

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,1 +1,8 @@
 #START
+
+# Implementing Insertion sort code.
+# Using the array values assigned to me.
+
+
+print("\n\t\t\t\t*******PROGRAMMED BY:*******")
+print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
