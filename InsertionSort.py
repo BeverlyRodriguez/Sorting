a727189b009d7e12fded91ab9767f3db60f9e3c1
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 07:43:42 +0800

    Def Insertion Sort and loops

--- InsertionSort.py
+++ InsertionSort.py
@@ -6,3 +6,21 @@
 
 print("\n\t\t\t\t*******PROGRAMMED BY:*******")
 print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
+
+
+print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
+
+def insertion_Sort(numbers):
+    for i in range(1,len(numbers)):
+        sorted_area = numbers[i]
+        j = i - 1
+        while j >= 0 and sorted_area < numbers[j]:
+            numbers[j+1] = numbers[j]
+            j = j - 1
+        numbers[j+1] = sorted_area
+        print(numbers)
+   
+
+numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
+insertion_Sort(numbers)
+print(numbers)
