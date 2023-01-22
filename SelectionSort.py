2ea097293d22b0af518c67781e6e3dfb5e297d0a
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-22 12:59:42 +0800

    Final Comments

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,4 +1,8 @@
-#Start
+# Implementing Selection sort code.
+# Using the array values assigned to me.
+
+
+
 print("\n\t\t\t\t*******PROGRAMMED BY:*******")
 print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
 
