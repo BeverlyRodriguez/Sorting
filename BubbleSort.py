21a9b90d3f5df3c786cdcb99dd16afffaca427a0
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-22 22:45:38 +0800

    Def function and For loop

--- BubbleSort.py
+++ BubbleSort.py
@@ -6,5 +6,14 @@ print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
 
 print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
 
+def sort(nums):
+
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+                
 numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
 sort(numbers)
