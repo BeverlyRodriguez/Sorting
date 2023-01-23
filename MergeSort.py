afc9f9f76d265bd11527fbd040b6dbc37a567281
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 08:13:53 +0800

    Def Merge Sort and Seperation

--- MergeSort.py
+++ MergeSort.py
@@ -8,4 +8,14 @@ print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
 print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
 
 
+def merge_sort (Numbers):
+    if len(Numbers) > 1:
+        # Dividing into two parts
+        LeftPart = Numbers[:len(Numbers)//2]
+        RightPart = Numbers[len(Numbers)//2:]
+
+        merge_sort(LeftPart)
+        merge_sort(RightPart)
+
+        
 numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
