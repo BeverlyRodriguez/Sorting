5c55bc399c0a7616edfc4a7e338fe9ccb3b19ecb
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 09:19:09 +0800

    Def Quick Sort and Loops

--- QuickSort.py
+++ QuickSort.py
@@ -7,6 +7,32 @@ print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
 
 
 print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
+def quicksort(numbers,left,right):
+    if left < right:
+        partition_pos = partition(numbers,left,right)
+        quicksort(numbers, left,  partition_pos - 1)
+        quicksort(numbers, partition_pos + 1, right)
+
+def partition(numbers, left, right):
+    i = left
+    j = right - 1
+    pivot = numbers[right]
+
+    while i < j:
+        while i < right and numbers[i] < pivot:
+            i += 1
+        while j > left and numbers[j] >= pivot:
+            j-=1
+
+        if i < j:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+    if numbers[i] > pivot:
+        numbers[i], numbers[right] = numbers[right], numbers[i]
+    print(numbers)
+    return i
+    
 
 
 numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
+quicksort(numbers, 0, len(numbers) - 1)
