228dd211159b1c00f415390f64622d3327cd744d
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 08:01:35 +0800

    Array Values

--- MergeSort.py
+++ MergeSort.py
@@ -4,3 +4,8 @@
 
 print("\n\t\t\t\t*******PROGRAMMED BY:*******")
 print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
+
+print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
+
+
+numbers = [60, 63, 96, 5, 90, 43, 75, 41, 99, 23]
