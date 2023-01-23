9eb2b5c2066871a0154e49d6013f5605b26e6223
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 08:00:07 +0800

    Adding Comments and Programmed by: Rodriguez

--- MergeSort.py
+++ MergeSort.py
@@ -1,1 +1,6 @@
-#START
+# Implementing Merge Sort code.
+# Using the array values assigned to me.
+
+
+print("\n\t\t\t\t*******PROGRAMMED BY:*******")
+print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
