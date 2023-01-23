0e993d419de6b8e1176b4550aeaeec24658fc649
Beverly Rodriguez <rodriguezbeverlyann758@gmail.com>
2023-01-23 08:38:47 +0800

    Adding Comments and Programmed by: Rodriguez

--- QuickSort.py
+++ QuickSort.py
@@ -1,1 +1,9 @@
-#START
+# Implementing Quick Sort code.
+# Using the array values assigned to me.
+
+
+print("\n\t\t\t\t*******PROGRAMMED BY:*******")
+print("\t\t\t\t**BEVERLY ANN L. RODRIGUEZ**\n")
+
+
+print("\t\t   Array values:[60, 63, 96, 5, 90, 43, 75, 41, 99, 23]\n")
